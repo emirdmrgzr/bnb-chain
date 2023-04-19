37134233d04030c59ee3a2fd88bd236449135af1
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-04-19 13:38:30 +0300

    Data Locations of Solidity

---
+++ 15_DataLocations.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+// Storage -> It is stored in the blockchain. State variables.
+// Memory -> Temporary data location. It is being cleared after function completed.
+// Calldata -> It's similar to memory but it contains function args.
+
+contract Example {
+    uint storedData; // stored in storage
+    string storedString; // stored in storage
+
+    function set(uint256 x, string calldata s) public {
+        storedData = x;
+        storedString = s;
+    }
+
+    function get() public view returns (uint, string memory) {
+        return (storedData, storedString); // read from storage;
+    }
+
+    function double(uint[] calldata nums) public pure returns (uint[] memory) {
+        uint[] memory result = new uint[](nums.length);
+
+        for (uint i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * 2;
+        }
+
+        return result;
+    }
+}
